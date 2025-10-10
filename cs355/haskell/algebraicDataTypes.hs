1319fd7e0ed6e359e0313dde3eb22fde36a9fcdd
Pushpendra <pushpendrauikey963@gmail.com>
2025-10-10 14:14:54 +0530

    Some more practice with Algebraic Data Types

--- cs355/haskell/algebraicDataTypes.hs
+++ cs355/haskell/algebraicDataTypes.hs
@@ -1,4 +1,4 @@
-data Shape = Circle Float | Rectangle Float Float
+-- data Shape = Circle Float | Rectangle Float Float
 
 area :: Shape -> Float
 area (Circle r)        = pi * r^2
@@ -13,3 +13,42 @@ safeHead (x:_) = Just x
 safeDivision :: Int -> Int -> Maybe Int 
 safeDivision _ 0 = Nothing
 safeDivision m n = Just (m `div` n)
+
+
+-- Recursive Types
+data Nat = Zero | Succ Nat  -- Church Numerals
+
+nat2int :: Nat -> Int
+nat2int Zero = 0
+nat2int (Succ n) = 1 + nat2int n
+
+data Shape = Circle Float | Rectangle Float Float
+
+instance Eq Shape where
+    (==) :: Shape -> Shape -> Bool
+    (Circle r1) == (Circle r2) = r1 == r2
+    (Rectangle l1 b1) == (Rectangle l2 b2) = l1 == l2 && b1 == b2
+    _ == _ = False
+
+-- class (Eq a, Show a) => Num a where 
+--   (+),(-),(*) :: a -> a -> a 
+--   negate :: a -> a 
+--   abs, signum :: a -> a  
+--   fromInteger :: Integer -> a 
+
+
+-- data Bool = False | True
+-- instance Eq Bool where 
+--     False == False = True 
+--     True  == True  = True 
+--     _     == _     = False
+
+-- This deriving method generates a definition; the "instance" one does not.
+-- data Student = Student { name :: String,
+--                          rollNum :: Int} deriving Show 
+
+data Student  = Student { name :: String, 
+                          rollNum :: Int}
+instance Show Student where
+  show :: Student -> String
+  show (Student n r) = "Student { name = " ++ show n ++ ", rollNum = " ++ show r ++ " }"
